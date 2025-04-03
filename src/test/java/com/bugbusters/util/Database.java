1f46dd1ee4d3fcb1ede6ee4d68b992f31951fa93
ptaylor9 <ptaylor9@madisoncollege.edu>
2025-04-03 17:58:10 -0500

    added propertiesLoader database.properties and log4j2.properties to test resources

---
+++ src/test/java/com/bugbusters/util/Database.java
@@ -0,0 +1,130 @@
+package com.bugbusters.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * Provides access to the database
+ * Created on 8/31/16.
+ *
+ * @author pwaite
+ * @author Alex M - Fall 2019 - added multi-line sql capability
+ */
+
+public class Database implements PropertiesLoader {
+
+    private static final Logger logger = LogManager.getLogger(Database.class);
+
+    // create an object of the class Database
+    private static Database instance = new Database();
+
+    private Properties properties;
+    private Connection connection;
+
+    /** private constructor prevents instantiating this class anywhere else
+     **/
+    public Database() {
+        init();
+    }
+
+    /** load the properties file containing the driver, connection url, userid and pwd.
+     * Using the loadProperties method from the PropertiesLoader interface
+     */
+    private void init() {
+        try {
+            properties = loadProperties("/database.properties");
+        } catch (Exception e) {
+            logger.error(e);
+        }
+    }
+
+    /** get the only Database object available
+     @return the single database object
+     */
+    public static Database getInstance() {
+        return instance;
+    }
+
+    /** get the database connection
+     @return the database connection
+     */
+    public Connection getConnection() {
+        return connection;
+    }
+
+    /** attempt to connect to the database
+     */
+    public void connect() throws Exception {
+        if (connection != null)
+            return;
+
+        try {
+            Class.forName(properties.getProperty("driver"));
+        } catch (ClassNotFoundException e) {
+            throw new Exception("Database.connect()... Error: MySQL Driver not found");
+        }
+
+        String url = properties.getProperty("url");
+        connection = DriverManager.getConnection(url, properties.getProperty("username"),  properties.getProperty("password"));
+    }
+
+    /** close and clean up the database connection
+     */
+    public void disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                logger.error(e);
+            }
+        }
+
+        connection = null;
+    }
+
+    /**
+     * Run the sql.
+     *
+     * @param sqlFile the sql file to be read and executed line by line
+     */
+    public void runSQL(String sqlFile) {
+
+        Statement stmt = null;
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(classloader.getResourceAsStream(sqlFile))))  {
+
+            connect();
+            stmt = connection.createStatement();
+
+            String sql = "";
+            while (br.ready())
+            {
+                char inputValue = (char)br.read();
+
+                if(inputValue == ';')
+                {
+                    stmt.executeUpdate(sql);
+                    sql = "";
+                }
+                else
+                    sql += inputValue;
+            }
+
+        } catch (SQLException se) {
+            logger.error(se);
+        } catch (Exception e) {
+            logger.error(e);
+        } finally {
+            disconnect();
+        }
+
+    }
+}
