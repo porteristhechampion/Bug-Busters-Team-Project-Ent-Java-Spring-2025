a63c4130d6a511b833a034df80c9f41218026d4b
ajfait <jafait1004@gmail.com>
2025-04-12 13:02:34 -0500

    deleted cat.sql and added cat_memes.sql

---
+++ dba/cat_memes.sql
@@ -0,0 +1,16 @@
+-- Create database
+DROP DATABASE IF EXISTS cat_memes;
+CREATE DATABASE IF NOT EXISTS cat_memes;
+-- Use database
+USE cat_memes;
+-- Drop tables
+DROP TABLE IF EXISTS memes;
+-- Create table
+CREATE TABLE IF NOT EXISTS memes
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    ur          TEXT NOT NULL,
+    text_top    VARCHAR(55),
+    text_bottom VARCHAR(55),
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
