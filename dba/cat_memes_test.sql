3da100b373ac036b37ebc934aec578ab4f41553b
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-04-16 13:43:56 -0500

    Updated SQL Scripts

--- dba/cat_memes_test.sql
+++ dba/cat_memes_test.sql
@@ -1,3 +1,20 @@
+-- Create database
+DROP DATABASE IF EXISTS cat_memes_test;
+CREATE DATABASE IF NOT EXISTS cat_memes_test;
+-- Use database
+USE cat_memes_test;
+-- Drop tables
+DROP TABLE IF EXISTS memes;
+-- Create table
+CREATE TABLE IF NOT EXISTS memes
+(
+id          INT AUTO_INCREMENT PRIMARY KEY,
+url          TEXT NOT NULL,
+text_top    VARCHAR(55),
+text_bottom VARCHAR(55),
+created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Drop tables
 DROP TABLE IF EXISTS memes;
 
