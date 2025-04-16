29add3337eca08c7bc97b11456619c8e74f4efae
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-04-16 13:49:26 -0500

    Added jdocs to Meme.java

--- src/main/java/com/bugbusters/webservice/entity/Meme.java
+++ src/main/java/com/bugbusters/webservice/entity/Meme.java
@@ -26,47 +26,102 @@ public class Meme {
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
+    /**
+     * Instantiates a new Meme.
+     */
     public Meme() {
     }
 
+    /**
+     * Instantiates a new Meme.
+     *
+     * @param url        the url
+     * @param textTop    the text top
+     * @param textBottom the text bottom
+     */
     public Meme(String url, String textTop, String textBottom) {
         this.url = url;
         this.textTop = textTop;
         this.textBottom = textBottom;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets url.
+     *
+     * @return the url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets url.
+     *
+     * @param url the url
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * Gets text top.
+     *
+     * @return the text top
+     */
     public String getTextTop() {
         return textTop;
     }
 
+    /**
+     * Sets text top.
+     *
+     * @param textTop the text top
+     */
     public void setTextTop(String textTop) {
         this.textTop = textTop;
     }
 
+    /**
+     * Gets text bottom.
+     *
+     * @return the text bottom
+     */
     public String getTextBottom() {
         return textBottom;
     }
 
+    /**
+     * Sets text bottom.
+     *
+     * @param textBottom the text bottom
+     */
     public void setTextBottom(String textBottom) {
         this.textBottom = textBottom;
     }
 
+    /**
+     * Gets created at.
+     *
+     * @return the created at
+     */
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
