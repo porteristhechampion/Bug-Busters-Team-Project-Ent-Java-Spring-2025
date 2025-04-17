e77527a8b15e805a57b160e0acaf74a408c86b52
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-04-16 23:11:37 -0500

    Changed LogTest to use junit

--- src/test/java/com/bugbusters/LogTest.java
+++ src/test/java/com/bugbusters/LogTest.java
@@ -2,13 +2,16 @@ package com.bugbusters;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
 
 public class LogTest {
     private static final Logger logger = LogManager.getLogger(LogTest.class);
 
-    public static void main(String[] args) {
+    @Test
+    void logAllLevels() {
         logger.debug("This is a DEBUG message.");
         logger.info("This is an INFO message.");
         logger.error("This is an ERROR message.");
+
     }
 }
