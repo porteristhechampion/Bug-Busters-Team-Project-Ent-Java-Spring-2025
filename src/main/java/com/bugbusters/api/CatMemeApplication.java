45151cbd5f79c51e9c3373cf313af5141cf8468e
Justin Gritton-Bell <jgrittonbell@madisoncollege.edu>
2025-04-17 21:16:21 -0500

    Refactored SwaggerConfig Entry Point into CatMemeApplication EntryPoint and simplified the web.xml to fix conflicts

--- src/main/java/com/bugbusters/api/CatMemeApplication.java
+++ src/main/java/com/bugbusters/api/CatMemeApplication.java
@@ -1,25 +1,29 @@
 package com.bugbusters.api;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-import java.util.HashSet;
-import java.util.Set;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
-@ApplicationPath("/api")
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title       = "Cat Meme Generator API",
+                version     = "1.0.0",
+                description = "Upload an image, add text overlays, store in S3 & the DB."
+        )
+)
 public class CatMemeApplication extends Application {
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-        // your resource(s)
-        classes.add(CatMemeResource.class);
-        // enable multipart/form-data
-        classes.add(MultiPartFeature.class);
-        // enable JSON <-> POJO marshalling
-        classes.add(JacksonFeature.class);
-        return classes;
+        return Set.of(
+                CatMemeResource.class,   // your endpoints
+                MultiPartFeature.class,   // enable multipart/form-data
+                JacksonFeature.class,     // JSON ↔ POJO
+                OpenApiResource.class     // generates /openapi.json
+        );
     }
 }
